83d3a6810234cb6285a986ea0df864f7504ac47e
Administrator <77>
2019-01-02 22:45:51 +0800

    2019年1月2日 22:40:19

--- app/src/main/java/org/chinasafety/liu/anjiantong/presenter/IHazardRegistrationPresenter.java
+++ app/src/main/java/org/chinasafety/liu/anjiantong/presenter/IHazardRegistrationPresenter.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3f0610cd3da0e186841c2ffe72825857c8d5d5c3f50c141a273769f5412b9b26
-size 1047
+oid sha256:4b3e1ace51fcea1abcb246ab08d0ec9f2b2155970da2e2d74dd860f2bdb8956c
+size 864
--- app/src/main/java/org/chinasafety/liu/anjiantong/presenter/impl/YhdjPresenterCompl.java
+++ app/src/main/java/org/chinasafety/liu/anjiantong/presenter/impl/YhdjPresenterCompl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2948b62b095b5d5272c58bc222bcfbcc9231b5a94f71063db3ff0a1be368d1df
-size 14988
+oid sha256:0919f626a28126eb1b0627df8b62c6687f78063ebb337b2b7a4d16bf4c302979
+size 10551
